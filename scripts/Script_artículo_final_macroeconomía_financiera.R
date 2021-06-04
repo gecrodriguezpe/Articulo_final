03874b649e3b88029520b90320d32a846cf01401
Germán Camilo Rodríguez <gecrodriguezpe@unal.edu.co>
2021-06-04 11:58:42 -0500

    modificaciones finales script. ya dieron significativos los factor risk premium

--- scripts/Script_artículo_final_macroeconomía_financiera.R
+++ scripts/Script_artículo_final_macroeconomía_financiera.R
@@ -5,6 +5,7 @@ library(readxl)
 library(dynlm)
 library(xts)
 library(stargazer)
+library(corrplot)
 
 ###
 # Modelo macro factorial para analizar el mercado accionario colombiano 
@@ -44,8 +45,14 @@ variables$DEI = diff(variables$Inflacion_pronosticada)
 # Term structure: UTS
 variables$UTS = variables$tasa_1_año_numero - variables$tasa_10_años_numero
 
+# Matriz de correlaciones entre los diferentes factores macroeconómicos que se usaran en las estimaciones
+variables_simple = as_tibble(variables) %>% 
+  select(MP, UI, DEI, UTS, colcap, brent_dolares)
 
-# 3. Regreiones ----
+source("http://www.sthda.com/upload/rquery_cormat.r")
+rquery.cormat(variables_simple)
+
+# 3. Regresiones ----
 
 # 3.1 Estimación de factor loadings por medio de una regresión de series de tiempo ----
 
@@ -107,25 +114,108 @@ market_petroleum = factor_loadings(completa, "market_petroleum")
 
 factor_risk_premium = function(factor_loadings, completa, tipo){
   # Se va a tomar como la tasa libre de riesgo el promedio de la tasa de los TES a 1 año durante el periodo estudiado 
-  riskless_return = mean(completa$tasa_1_año_numero)
-  mean_return = c()
-  for (equity in 1:15){
-    mean_i = mean(completa[,equity], na.rm = TRUE)
-    mean_return = append(mean_return, mean_i)
-  }
-  factor_loadings$risk_premia = mean_return - riskless_return
+  completa2 = as_tibble(completa)
+  prov = factor_loadings
   if (tipo == "none"){
-    reg = lm(risk_premia ~ MP_fl + UI_fl + DEI_fl + UTS_fl, factor_loadings)
-    return(reg)
+    coef = tibble(constant = double(), MP_fl = double(), UI_fl = double(), DEI_fl = double(), UTS_fl = double())
+    t_values = tibble(constant = double(), MP_fl = double(), UI_fl = double(), DEI_fl = double(), UTS_fl = double())
+    for (month in 2:nrow(completa2)){
+      vector_returns = c()
+      for (equity in 1:15){
+        vector_returns = append(vector_returns, completa2[month,equity])  
+      }
+      prov$month_var = as.double(vector_returns)
+      reg = lm(month_var ~ MP_fl + UI_fl + DEI_fl + UTS_fl, prov) 
+      other_month = month - 1
+      coef[other_month,] = t(summary(reg)$coefficients[,1])
+      t_values[other_month,] = t(summary(reg)$coefficients[,3])
+    }
+    final = data.frame(constant = double(), MP_fl = double(), UI_fl = double(), DEI_fl = double(), UTS_fl = double())
+    mean_factor = c()
+    mean_t = c()
+    for (factor in 1:ncol(coef)){
+      mean_factor = append(mean_factor, mean(coef[[factor]], na.rm = TRUE))
+      mean_t = append(mean_t, mean(t_values[[factor]], na.rm = TRUE))
+    }
+    final[1, ] = t(as.double(mean_factor))
+    final[2, ] = t(as.double(mean_t))
+    row.names(final) = c("value", "t")
+    return(final)
   }else if (tipo == "market"){
-    reg = lm(risk_premia ~ MP_fl + UI_fl + DEI_fl + UTS_fl + colcap, factor_loadings)
-    return(reg)
+    coef = tibble(constant = double(), MP_fl = double(), UI_fl = double(), DEI_fl = double(), UTS_fl = double(), colcap = double())
+    t_values = tibble(constant = double(), MP_fl = double(), UI_fl = double(), DEI_fl = double(), UTS_fl = double(), colcap = double())
+    for (month in 2:nrow(completa2)){
+      vector_returns = c()
+      for (equity in 1:15){
+        vector_returns = append(vector_returns, completa2[month,equity])  
+      }
+      prov$month_var = as.double(vector_returns)
+      reg = lm(month_var ~ MP_fl + UI_fl + DEI_fl + UTS_fl + colcap, prov) 
+      other_month = month - 1
+      coef[other_month,] = t(summary(reg)$coefficients[,1])
+      t_values[other_month,] = t(summary(reg)$coefficients[,3])
+    }
+    final = data.frame(constant = double(), MP_fl = double(), UI_fl = double(), DEI_fl = double(), UTS_fl = double(), colcap = double())
+    mean_factor = c()
+    mean_t = c()
+    for (factor in 1:ncol(coef)){
+      mean_factor = append(mean_factor, mean(coef[[factor]], na.rm = TRUE))
+      mean_t = append(mean_t, mean(t_values[[factor]], na.rm = TRUE))
+    }
+    final[1, ] = t(as.double(mean_factor))
+    final[2, ] = t(as.double(mean_t))
+    row.names(final) = c("value", "t")
+    return(final)
   }else if (tipo == "petroleum"){
-    reg = lm(risk_premia ~ MP_fl + UI_fl + DEI_fl + UTS_fl + brent, factor_loadings)
-    return(reg)
+    coef = tibble(constant = double(), MP_fl = double(), UI_fl = double(), DEI_fl = double(), UTS_fl = double(), brent = double())
+    t_values = tibble(constant = double(), MP_fl = double(), UI_fl = double(), DEI_fl = double(), UTS_fl = double(), brent = double())
+    for (month in 2:nrow(completa2)){
+      vector_returns = c()
+      for (equity in 1:15){
+        vector_returns = append(vector_returns, completa2[month,equity])  
+      }
+      prov$month_var = as.double(vector_returns)
+      reg = lm(month_var ~ MP_fl + UI_fl + DEI_fl + UTS_fl + brent, prov) 
+      other_month = month - 1
+      coef[other_month,] = t(summary(reg)$coefficients[,1])
+      t_values[other_month,] = t(summary(reg)$coefficients[,3])
+    }
+    final = data.frame(constant = double(), MP_fl = double(), UI_fl = double(), DEI_fl = double(), UTS_fl = double(), brent = double())
+    mean_factor = c()
+    mean_t = c()
+    for (factor in 1:ncol(coef)){
+      mean_factor = append(mean_factor, mean(coef[[factor]], na.rm = TRUE))
+      mean_t = append(mean_t, mean(t_values[[factor]], na.rm = TRUE))
+    }
+    final[1, ] = t(as.double(mean_factor))
+    final[2, ] = t(as.double(mean_t))
+    row.names(final) = c("value", "t")
+    return(final)
   }else if (tipo == "market_petroleum"){
-    reg = lm(risk_premia ~ MP_fl + UI_fl + DEI_fl + UTS_fl + colcap + brent, factor_loadings)
-    return(reg)
+    coef = tibble(constant = double(), MP_fl = double(), UI_fl = double(), DEI_fl = double(), UTS_fl = double(), colcap = double(), brent = double())
+    t_values = tibble(constant = double(), MP_fl = double(), UI_fl = double(), DEI_fl = double(), UTS_fl = double(), colcap = double(), brent = double())
+    for (month in 2:nrow(completa2)){
+      vector_returns = c()
+      for (equity in 1:15){
+        vector_returns = append(vector_returns, completa2[month,equity])  
+      }
+      prov$month_var = as.double(vector_returns)
+      reg = lm(month_var ~ MP_fl + UI_fl + DEI_fl + UTS_fl + colcap + brent, prov) 
+      other_month = month - 1
+      coef[other_month,] = t(summary(reg)$coefficients[,1])
+      t_values[other_month,] = t(summary(reg)$coefficients[,3])
+    }
+    final = data.frame(constant = double(), MP_fl = double(), UI_fl = double(), DEI_fl = double(), UTS_fl = double(), colcap = double(), brent = double())
+    mean_factor = c()
+    mean_t = c()
+    for (factor in 1:ncol(coef)){
+      mean_factor = append(mean_factor, mean(coef[[factor]], na.rm = TRUE))
+      mean_t = append(mean_t, mean(t_values[[factor]], na.rm = TRUE))
+    }
+    final[1, ] = t(as.double(mean_factor))
+    final[2, ] = t(as.double(mean_t))
+    row.names(final) = c("value", "t")
+    return(final)
   }
 }
 
@@ -133,13 +223,17 @@ factor_risk_premium = function(factor_loadings, completa, tipo){
 
 # 1. Sin petroleo ni market
 none_risk_p = factor_risk_premium(none, completa, "none")
-# 2. Con market sin petroleo
+# # 2. Con market sin petroleo
 market_risk_p = factor_risk_premium(market, completa, "market")
-# 3. Con petroleo sin market 
+# # 3. Con petroleo sin market 
 petroleum_risk_p = factor_risk_premium(petroleum, completa, "petroleum")
-# 4. Con market con petroleo
+# # 4. Con market con petroleo
 market_petroleum_risk_p = factor_risk_premium(market_petroleum, completa, "market_petroleum")
-
-# 4. Presentación de resultados ----
-
-
+# 
+# # 4. Presentación de resultados ----
+# 
+# stargazer(none_risk_p, market_risk_p, petroleum_risk_p, market_petroleum_risk_p, type = "latex",
+#           title = "Primas de riesgo para los factores macroeconómicos seleccionados", keep.stat = c("n", "rsq"),
+#           style = "AER")
+# 
+# 
